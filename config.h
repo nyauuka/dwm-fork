4b9d7868b9c60051341163170624ddef0746c905
nyauuka <maillnet@bk.ru>
2025-09-18 13:05:08 +0300

    screenshots for rep

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 6;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd:size=24", "Twemoji:size=24"};
-static const char dmenufont[]       = "JetBrainsMono Nerd:size=24";
+static const char *fonts[]          = { "JetBrainsMono Nerd:size=12", "Twemoji:size=12"};
+static const char dmenufont[]       = "JetBrainsMono Nerd:size=12";
 /* static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
